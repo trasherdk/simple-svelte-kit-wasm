5c0d3f1ae4deeed67f6b2df6229636ebc97944f5
TrasherDK <trasherdk@fumlersoft.dk>
2022-09-19 04:12:30 +0200

    Layout: add Header nav stuff

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,38 @@
+<header>
+  <nav>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/monero-head">Monero Head</a></li>
+      <li><a href="/monero-dynamic">Monero Dynamic</a></li>
+      <li><a href="/monero-import">Monero Import</a></li>
+      <li><a href="/ssr">WASM with SSR</a></li>
+      <li><a href="/no-ssr">WASM without SSR</a></li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: center;
+    --background: rgba(255, 255, 255, 0.7);
+  }
+  ul {
+    position: relative;
+    padding: 0;
+    margin: 0;
+    height: 3em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+    background: var(--background);
+    background-size: contain;
+  }
+
+  li {
+    position: relative;
+    height: 100%;
+    padding: 2rem;
+  }
+</style>
